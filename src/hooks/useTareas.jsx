4ce43e1b91d700451f12f926a1e4fe65c85e778f
Diego Salazar <diegosalazargonzalez2017@gmail.com>
2023-01-12 19:52:41 -0500

    deployment

--- src/hooks/useTareas.jsx
+++ src/hooks/useTareas.jsx
@@ -1,6 +1,5 @@
 import { useContext } from "react";
-import TareasContext from "../context/tareasProvider";
-
+import TareasContext from '../context/TareasProvider';
 const useTareas = () =>{
     return useContext(TareasContext)
 }
